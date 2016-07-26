74d56d4013bf5ed71140f9701a6970863efefbf0
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-07-26 12:06:09 -0400

    Add more detail to libnv test.
    Inspect the final debug output as well as the instrumented IR.

--- test/ktrace-userspace-libnv.c
+++ test/ktrace-userspace-libnv.c
@@ -6,8 +6,11 @@
  * RUN: %cpp -DPOLICY_FILE %s > %t.yaml
  * RUN: %cpp %cflags %s > %t.c
  * RUN: %clang %cflags -S -emit-llvm %cflags %t.c -o %t.ll
- * RUN: %loom -S %t.ll -loom-file %t.yaml -loom-ktrace=utrace -loom-serialization=nv -o %t.instr.ll
+ * RUN: %loom -S %t.ll -loom-file %t.yaml -loom-ktrace=utrace -loom-serialization=nv -loom-nv-debug -o %t.instr.ll
  * RUN: %filecheck -input-file %t.instr.ll %s
+ * RUN: %clang -lnv %t.instr.ll -o %t.instr
+ * RUN: %t.instr 2> %t.instr.out
+ * RUN: %filecheck -input-file %t.instr.out -check-prefix=CHECK-OUTPUT %s
  */
 
 #if defined (POLICY_FILE)
@@ -16,7 +19,7 @@ hook_prefix: __ktrace_test
 
 functions:
     - name: foo
-      caller: [ entry ]
+      caller: [ exit ]
 
 #else
 
@@ -28,13 +31,19 @@ int	foo(int x, float y, double z)
 	return x;
 }
 
-// CHECK:       define{{.*}} void @__ktrace_test_call_foo
+// CHECK:       define{{.*}} void @__ktrace_test_return_foo
 // CHECK-NEXT:  preamble:
 // CHECK:       [[LIST:%.*]] = call %nvlist_t* @nvlist_create
 // CHECK:       [[BUFFER:%.*]] = call i8* @nvlist_pack(%nvlist_t* [[LIST]], i64* [[LENPTR:%.*]])
 // CHECK-DAG:   [[LEN:%.*]] = load i64, i64* [[LENPTR]]
 // CHECK-DAG:   call void @nvlist_destroy(%nvlist_t* [[LIST]])
 // CHECK:       call i32 @utrace(i8* [[BUFFER]], i64 [[LEN]])
+
+// CHECK-OUTPUT:      name (STRING): [__ktrace_test_return_foo]
+// CHECK-OUTPUT-NEXT: description (STRING): [return foo:]
+// CHECK-OUTPUT-NEXT: values (NVLIST):
+// CHECK-OUTPUT-NEXT:   (NUMBER): {{.*}} (1) (0x1)
+// CHECK-OUTPUT-NEXT:   (NUMBER): {{.*}} (1) (0x1)
 
 int
 main(int argc, char *argv[])
