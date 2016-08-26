529d78a5343738aa6d3626395fa4cfa82653d7c3
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-08-26 12:18:51 -0230

    Refactor utrace test into two parts.
    Let there be two ktrace/utrace tests: one that inspects the generated
    instrumentation and can thus be run on any platform and one that actually
    runs the resulting instrumentation and can thus only run on platforms with a
    recent libnv (i.e., FreeBSD 11+).

---
+++ test/ktrace-userspace-libnv-run.c
@@ -0,0 +1,53 @@
+/**
+ * \file  ktrace-userspace-libnv-run.c
+ * \brief Actually run utrace instrumentation with libnv serialization.
+ *
+ * FreeBSD <11 doesn't provide the libnv we're looking for in base or ports:
+ * XFAIL: darwin, freebsd9, freebsd10, linux
+ *
+ * Commands for llvm-lit:
+ * RUN: %cpp -DPOLICY_FILE %s > %t.yaml
+ * RUN: %cpp %cflags %s > %t.c
+ * RUN: %clang -target x86_64-unknown-freebsd %cflags -S -emit-llvm %cflags %t.c -o %t.ll
+ * RUN: %loom -S %t.ll -loom-file %t.yaml -loom-nv-debug -o %t.instr.ll
+ * RUN: %clang -lnv %t.instr.ll -o %t.instr
+ * RUN: %t.instr 2> %t.instr.out
+ * RUN: %filecheck -input-file %t.instr.out %s
+ */
+
+#if defined (POLICY_FILE)
+
+hook_prefix: __ktrace_test
+
+ktrace: utrace
+
+serialization: nv
+
+functions:
+    - name: foo
+      caller: [ exit ]
+
+#else
+
+#include <stdio.h>
+
+int	foo(int x, float y, double z)
+{
+	return x;
+}
+
+// CHECK:      name (STRING): [__ktrace_test_return_foo]
+// CHECK-NEXT: description (STRING): [return foo:]
+// CHECK-NEXT: values (NVLIST):
+// CHECK-NEXT:   (NUMBER): {{.*}} (1) (0x1)
+// CHECK-NEXT:   (NUMBER): {{.*}} (1) (0x1)
+
+int
+main(int argc, char *argv[])
+{
+	foo(1, 2, 3);
+
+	return 0;
+}
+
+#endif /* !POLICY_FILE */
--- test/ktrace-userspace-libnv.c
+++ test/ktrace-userspace-libnv.c
@@ -2,18 +2,12 @@
  * \file  ktrace-userspace-libnv.c
  * \brief Tests userspace ktrace instrumentation with libnv serialization.
  *
- * FreeBSD <11 doesn't provide the libnv we're looking for in base or ports:
- * XFAIL: freebsd9, freebsd10
- *
  * Commands for llvm-lit:
  * RUN: %cpp -DPOLICY_FILE %s > %t.yaml
  * RUN: %cpp %cflags %s > %t.c
- * RUN: %clang %cflags -S -emit-llvm %cflags %t.c -o %t.ll
+ * RUN: %clang -target x86_64-unknown-freebsd %cflags -S -emit-llvm %cflags %t.c -o %t.ll
  * RUN: %loom -S %t.ll -loom-file %t.yaml -loom-nv-debug -o %t.instr.ll
  * RUN: %filecheck -input-file %t.instr.ll %s
- * RUN: %clang -lnv %t.instr.ll -o %t.instr
- * RUN: %t.instr 2> %t.instr.out
- * RUN: %filecheck -input-file %t.instr.out -check-prefix=CHECK-OUTPUT %s
  */
 
 #if defined (POLICY_FILE)
@@ -45,12 +39,6 @@ int	foo(int x, float y, double z)
 // CHECK-DAG:   [[LEN:%.*]] = load i64, i64* [[LENPTR]]
 // CHECK-DAG:   call void @nvlist_destroy(%nvlist_t* [[LIST]])
 // CHECK:       call i32 @utrace(i8* [[BUFFER]], i64 [[LEN]])
-
-// CHECK-OUTPUT:      name (STRING): [__ktrace_test_return_foo]
-// CHECK-OUTPUT-NEXT: description (STRING): [return foo:]
-// CHECK-OUTPUT-NEXT: values (NVLIST):
-// CHECK-OUTPUT-NEXT:   (NUMBER): {{.*}} (1) (0x1)
-// CHECK-OUTPUT-NEXT:   (NUMBER): {{.*}} (1) (0x1)
 
 int
 main(int argc, char *argv[])
