1ab122219ca65406567981e5f31e95f5b77c5c89
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-08-31 22:59:07 -0230

    Add DebugInfo class to parse LLVM debug info.
    This class provides useful methods for tracing backwards and forwards
    through LLVM Metadata (which is currently only used to store debug info).
    This short-circuits the DWARF vs CTF discussion by going directly to the
    information that DWARF output is generated from (and, in point of fact,
    which CTF could be generated from if we chose to do so).

---
+++ src/DebugInfo.cc
@@ -0,0 +1,157 @@
+//! @file DebugInfo.cc  Definition of DebugInfo class.
+/*
+ * Copyright (c) 2016 Jonathan Anderson
+ * All rights reserved.
+ *
+ * This software was developed by BAE Systems, the University of Cambridge
+ * Computer Laboratory, and Memorial University under DARPA/AFRL contract
+ * FA8650-15-C-7558 ("CADETS"), as part of the DARPA Transparent Computing
+ * (TC) research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "DebugInfo.hh"
+
+#include <llvm/IR/DebugInfoMetadata.h>
+#include <llvm/IR/IntrinsicInst.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/raw_ostream.h>
+
+using namespace llvm;
+using namespace loom;
+
+
+DebugInfo::DebugInfo(llvm::Module& M)
+  : Mod(M), DbgDeclare(Mod.getFunction("llvm.dbg.declare"))
+{
+  Mod.getMDKindNames(MetadataKinds);
+
+  if (DbgDeclare) {
+    // Examine all calls to @llvm.dbg.declare() to find metadata:
+    for (auto& Use : DbgDeclare->uses()) {
+      auto *Dbg = dyn_cast<DbgDeclareInst>(Use.getUser());
+      assert(Dbg && "call to llvm.dbg.declare must be a DbgDeclareInst");
+      DbgDecls[Dbg->getAddress()].push_back(Dbg->getVariable());
+    }
+  }
+}
+
+
+bool DebugInfo::ModuleHasFullDebugInfo() const
+{
+  if (not DbgDeclare)
+    return false;
+
+  auto *ModuleMetadata = Mod.getNamedMetadata("llvm.dbg.cu");
+  if (not ModuleMetadata)
+    return false;
+
+  if (auto *DI = Get<DICompileUnit>(ModuleMetadata)) {
+    using DICompileUnit::DebugEmissionKind::FullDebug;
+
+    return DI->getEmissionKind() == FullDebug and not DI->isOptimized();
+  }
+
+  return false;
+}
+
+
+std::string DebugInfo::FieldName(GetElementPtrInst *GEP)
+{
+  // Trace back to a variable with debug metadata.
+  SmallVector<size_t, 4> GEPOffsets;
+  const DIVariable *Var = Trace(GEP, GEPOffsets);
+  if (Var == nullptr) {
+    errs() << "WARNING: unable to trace GEP ";
+    GEP->print(errs(), true);
+    errs() << " back to a variable\n";
+
+    return "";
+  }
+
+  const DITypeRef &T = Var->getType();
+  const DICompositeType *CT = dyn_cast<DICompositeType>(T);
+
+  while (CT) {
+    switch (CT->getTag()) {
+    case dwarf::DW_TAG_array_type:
+      CT = dyn_cast<DICompositeType>(CT->getBaseType());
+      break;
+
+    case dwarf::DW_TAG_structure_type: {
+      assert(not GEPOffsets.empty());
+
+      size_t Index = GEPOffsets.back();
+      GEPOffsets.pop_back();
+
+      auto *Member = dyn_cast<DIDerivedType>(CT->getElements()[Index]);
+
+      if (GEPOffsets.empty()) {
+        return Member->getName();
+      } else {
+        CT = dyn_cast<DICompositeType>(Member->getBaseType());
+      }
+
+      break;
+    }
+
+    default:
+      assert(false && "unexpected tag for DICompositeType");
+    }
+  }
+
+  return "";
+}
+
+
+const DIVariable*
+DebugInfo::Trace(GetElementPtrInst *GEP, SmallVectorImpl<size_t> &Offsets)
+{
+  // Walk backwards from GEP to source until we find a variable with
+  // debug metadata (or die trying, having reached the end of the GEP chain).
+  do
+  {
+    // If GEP'ing the address of a structure field, track which field
+    // index we are working with. 
+    if (isa<StructType>(GEP->getSourceElementType())
+        and GEP->getNumIndices() == 2) {
+
+      Value *Index = GEP->idx_begin()[1];
+
+      auto *ConstIdx = dyn_cast<ConstantInt>(Index);
+      assert(ConstIdx && "struct field indexing must be static");
+      Offsets.push_back(ConstIdx->getLimitedValue());
+    }
+
+    Value *Ptr = GEP->getPointerOperand()->stripPointerCasts();
+    if (auto *Var = Get<DIVariable>(Ptr)) {
+      return Var;
+    }
+
+    GEP = dyn_cast<GetElementPtrInst>(Ptr);
+  }
+  while (GEP != nullptr);
+
+  return nullptr;
+}
+
---
+++ src/DebugInfo.hh
@@ -0,0 +1,134 @@
+//! @file DebugInfo.h  Declaration of DebugInfo class.
+/*
+ * Copyright (c) 2016 Jonathan Anderson
+ * All rights reserved.
+ *
+ * This software was developed by BAE Systems, the University of Cambridge
+ * Computer Laboratory, and Memorial University under DARPA/AFRL contract
+ * FA8650-15-C-7558 ("CADETS"), as part of the DARPA Transparent Computing
+ * (TC) research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef LOOM_DEBUG_INFO_H
+#define LOOM_DEBUG_INFO_H
+
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Metadata.h>
+#include <llvm/IR/ValueMap.h>
+#include <llvm/Support/raw_ostream.h>
+
+
+namespace llvm {
+class GetElementPtrInst;
+}
+
+namespace loom {
+
+/// Class for finding LLVM debug information within a Module.
+class DebugInfo
+{
+public:
+  /// A single LLVM metadata node, with an integer metadata "kind".
+  typedef std::pair<unsigned, llvm::MDNode*> DebugNode;
+
+  /// A vector of metadata nodes associated with a Value.
+  template<unsigned int Size=4>
+  using DebugVec = llvm::SmallVector<DebugNode, Size>;
+
+  DebugInfo(llvm::Module&);
+
+  bool ModuleHasFullDebugInfo() const;
+
+  /// Find the name of a field being looked up by a GetElementPtrInst.
+  std::string FieldName(llvm::GetElementPtrInst*);
+
+  template<class DebugType = llvm::Metadata>
+  const DebugType* Get(llvm::NamedMDNode *Node) const
+  {
+    for (auto *MD : Node->operands()) {
+      if (auto *DebugInfo = llvm::dyn_cast<DebugType>(MD)) {
+        return DebugInfo;
+      }
+    }
+
+    return nullptr;
+  }
+
+  template<class DebugType = llvm::Metadata>
+  const DebugType* Get(llvm::Value *V) const
+  {
+    if (auto *I = llvm::dyn_cast<llvm::Instruction>(V)) {
+      llvm::SmallVector<DebugNode, 4> Debug;
+      I->getAllMetadata(Debug);
+
+      for (auto i : Debug) {
+        if (auto *DebugInfo = llvm::dyn_cast<DebugType>(i.second)) {
+          return DebugInfo;
+        }
+      }
+    }
+
+    auto i = DbgDecls.find(V);
+    if (i != DbgDecls.end()) {
+      for (auto *MD : i->second) {
+        if (auto *DebugInfo = llvm::dyn_cast<DebugType>(MD)) {
+          return DebugInfo;
+        }
+      }
+    }
+
+    return nullptr;
+  }
+
+private:
+  /**
+   * Trace from a GetElementPtrInst representing a structure field lookup
+   * back to a variable that has a DIVariable debug metadata node.
+   *
+   * The result is a DIVariable for the variable with debug information and
+   * a vector that of GEP offsets, representing how we got to a variable from
+   * the GEP of interest. Together, these can be used to work forwards again
+   * from the DIVariable to a field name in the current GEP.
+   *
+   * @param[in]   GEP        the structure field lookup to trace
+   * @param[out]  Offsets    the vector to fill with GEP offsets
+   *
+   * @returns     debug metadata for the variable we found
+   */
+  const llvm::DIVariable* Trace(llvm::GetElementPtrInst *GEP,
+                                llvm::SmallVectorImpl<size_t> &Offsets);
+
+  llvm::Module& Mod;
+  llvm::Function *DbgDeclare;
+
+  /// Kinds of metadata that are used in this module.
+  llvm::SmallVector<llvm::StringRef, 4> MetadataKinds;
+
+  /// Declarations of metadata, i.e., metadata from `@llvm.db.declare()` calls.
+  llvm::ValueMap<llvm::Value*, llvm::SmallVector<llvm::Metadata*, 4>> DbgDecls;
+};
+
+} // namespace loom
+
+#endif // LOOM_DEBUG_INFO_H
