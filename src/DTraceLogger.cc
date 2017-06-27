5c336ca3905b3dd3cbebed9ccc18898c50b6f724
Brian Kidney <bkidney@briankidney.ca>
2017-06-27 14:52:04 -0230

    Handles variable arg lengths and pointer args.

--- src/DTraceLogger.cc
+++ src/DTraceLogger.cc
@@ -32,6 +32,8 @@
 
 #include "DTraceLogger.hh"
 
+#include <algorithm>
+
 #include <llvm/IR/Instructions.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/TypeBuilder.h>
@@ -53,19 +55,35 @@ Value* DTraceLogger::Log(Instruction *I, ArrayRef<Value*> Values,
 
   LLVMContext &Ctx = Mod.getContext();
   Type* param = TypeBuilder<uintptr_t, false>::get(Ctx);
-  std::vector<Type*> params = { param, param, param, param, param, param };
+
+  size_t n_args = std::min(Values.size(), 6ul);
+
+  std::vector<Type*> params;
+  for (int i = 0; i < n_args; i++) {
+      params.push_back(param);
+  }
+  
   auto *FT = FunctionType::get( TypeBuilder<int, false>::get(Ctx), params, false);
   
   Constant *F = Mod.getOrInsertFunction("dt_probe", FT);
 
+
   Value* args[6];
-  for (int i = 0; i < 6; i++)
+  for (int i = 0; i < n_args; i++)
   {
-      Value* ptr = ConstantInt::get( param , 0 );
-      args[i] = ptr;
+    Value *ptr;
+
+    Type *T = Values[i]->getType();
+    if (T->isPointerTy()) {
+        ptr = B.CreatePtrToInt(Values[i], param);
+    } else {
+      ptr = ConstantInt::get( param , 0 );
+    }
+    
+    args[i] = ptr;
   }
 
-  ArrayRef<Value*> argsRef(args, 6);
+  ArrayRef<Value*> argsRef(args, n_args);
 
   return B.CreateCall(F, argsRef);
 
